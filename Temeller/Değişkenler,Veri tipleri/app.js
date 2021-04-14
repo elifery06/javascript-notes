a02cb61a16e33db1f0acf9b57842de3a627d9390
ELİF ERYILDIRIM <75325359+elifery06@users.noreply.github.com>
2021-04-14 11:21:03 +0300

    Add files via upload

---
+++ Temeller/Değişkenler,Veri tipleri/app.js
@@ -0,0 +1,101 @@
+//Değişken oluşturma
+
+// var a=20;
+// var b=10;
+// var c=40;
+//  console.log(a,b,c);
+
+//primitive
+
+// var a =10; //number veri tipi
+// var b=3.14; //number veri tipi
+// console.log(typeof a);
+// console.log(a);
+// console.log(typeof b);
+// console.log(b);
+
+//string
+// var name="Mustafa";
+// console.log(name);
+// console.log(typeof name);
+
+//Boolean
+
+// var a =true;
+// console.log(typeof a);
+
+//null=a 'nın hiçbir değer taşımadığıbı gözsterir
+
+// var a =null;
+
+// console.log(a);
+
+// console.log(typeof a);
+
+//undefined
+
+// var a;
+
+// console.log(a);
+
+// console.log(typeof a);
+
+
+// var numbers=[1,2,3,4,5];
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[0]);
+
+// var person={
+//     name:"Elif",
+//     age:19
+// }
+// console.log(person);
+// console.log(typeof person);
+
+// var date=new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// var merhaba=function(){
+//     console.log("Merhaba Dünya");
+//     }
+// console.log(merhaba);
+// console.log(typeof merhaba);
+
+// var a =10;
+// var b=a;
+// console.log(a,b);
+//   a=20;
+//   console.log(a,b);
+
+// var a =[1,2,3];
+// var b=a;  
+// a.push(4);
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
