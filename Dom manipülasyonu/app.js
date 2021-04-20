6ade56156a6947ba76f22c511d4bd16fed8559b0
ELİF ERYILDIRIM <75325359+elifery06@users.noreply.github.com>
2021-04-20 23:36:39 +0300

    Add files via upload

---
+++ Dom manipülasyonu/app.js
@@ -0,0 +1,23 @@
+//DOM manipülasyonu statik sayfalara interaktiflik ve dinamiklik katmaya yarıyor
+
+// DOM=DOCUMENT OBJECT MODEL
+
+// HtML etikleri bir araya gelerek döküman obje modelini oluşturur.
+// Biz bu döküman üzerine ise elementlerimizi JavaScript ile manipüle edebiliriz.
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
