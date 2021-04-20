3da11365c46875bc0325a929cd9771c352cb4231
ELİF ERYILDIRIM <75325359+elifery06@users.noreply.github.com>
2021-04-20 23:38:40 +0300

    Add files via upload

---
+++ Temeller/Temeller/Scope kavramı/app.js
@@ -0,0 +1,108 @@
+//scope=kapsam= Bir değişkenin neelerde varolabileceğini söyler
+
+//Global scop=herhangi fonksiyon içinde yada herhangi bir if bloğu içinde olmayan scope
+
+// var value1=10;
+// let value2=20;
+// const value3=30;//tekrar atama yapılmıyor bir değişkene 
+
+// function func(){
+//     var value1=40;
+//     let value2=50;
+//     const value3=60;
+
+//     console.log(value1,value2,value3);
+// }
+// func();
+// console.log(value1,value2,value3);
+
+
+// if(true){
+//     var value1 =40;
+//     let value2=50;
+//     const value3=60;
+//     console.log(value1,value2,value3);
+// }
+
+// console.log(value1,value2,value3);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+//var değişkeni blok dışındada var olur
+//ancak let ve const değişkenleri oluşturulduğu bloğun dışında var olamazlar
+
+
+
+// function a(){
+//     //function scope'u
+// }
+
+
+// if(){
+//     //Blok skope'u
+// }
+
+
+
+// var database="123456";//var değişkeninin değeri if bloğunun içindeki değer olarak dğiştiirldi
+
+// if(true){
+//     var database="34343434";
+// //DEĞİŞTİRİLMESİNİ İSTEMEDİĞİMİZ DEĞİŞKENİ VAR İLE TANIMLAYAMAYIZ
+// }
+
+
+// const a="123456";
+
+// if(true){
+//     const a="34343434";
+// }
+
+// let a="123456";
+
+// if(true){
+//     let a="34343434";
+// }
+
+//CONST VE LETTE DEĞİŞKENİN DEĞERİNİ DEĞİŞTİRDİĞİNİZ ZAMAN DIŞARDAKİNİ ETKİLEMEZ
+//BU YÜZDEN VAR KULLNNNMAAAAAA!!!!!! GÜVENLİK İÇİN...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Temeller/Temeller/Windows Objesi/app.js
@@ -0,0 +1,88 @@
+//console.log(this);
+//console.log(this.alert("Merhaba canım"));
+
+//window objesi js de en genel objedir
+// console.log(window);
+// console.log(window.alert("merhaba tatlım"));
+
+
+//Alert
+//alert("merhaba");
+
+
+
+// // console.log(cevap);
+// if(confirm("Emin misiniz?")){
+//     console.log("Sil gitsin");
+// }
+// else{
+//     console.log("Silmeyin");
+// }
+
+
+
+
+// const cevap=prompt("2+2=?");
+// // console.log(cevap);
+// // console.log(typeof cevap);
+
+// if(cevap=="4"){
+//     console.log("Cevabınız doğru");
+// }
+// else{
+//     console.log("Cevabınız yanlış");
+// }
+
+
+
+let value;
+value=window;
+value=window.location;
+value=window.location.host;
+value=window.location.hostname;
+value=window.location.port;
+value=window.location.href;
+
+
+// if(confirm("Sayfa yenilensin mi?")){
+//     window.location.reload();
+// }
+// else{
+//     console.log("Sayfa yenilenmedi");
+// }
+
+value=window.outerHeight;//o pencerenin opikselini hesaplıyor küçültürsen kültür
+value=window.outerWidth;
+
+
+value=window.innerHeight;
+value=window.innerWidth;
+
+value=window.scrollX;//ne kadar sağ tarafa kaydıırlmış çubuk onu görüyosun
+value=window.scrollY;//ne kadar sağ tarafa kaydıırlmış çubuk onu görüyosun
+
+
+console.log(value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
